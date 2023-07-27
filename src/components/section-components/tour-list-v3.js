e1d50e4eb6231c06928903a82dbe0d3aff52ba3e
Akash5908 <venomkr020gmail.com>
2023-07-27 19:08:54 +0530

    sdadas

--- src/components/section-components/tour-list-v3.js
+++ src/components/section-components/tour-list-v3.js
@@ -1,71 +1,61 @@
-import React, { Component, useState } from "react"
-import ImageLazyLoad from "../section-components/ImageLazyLoad"
-import { Link } from "react-router-dom"
-import parse from "html-react-parser"
+import React, { Component } from "react";
+import ImageLazyLoad from "../section-components/ImageLazyLoad";
+import { Link } from "react-router-dom";
+import parse from "html-react-parser";
 
 class TourListV3 extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       query: "",
       locationFilter: "",
       priceFilter: null,
-    }
+    };
   }
 
   handleSearch = (e) => {
-    this.setState({ query: e.target.value })
-  }
+    this.setState({ query: e.target.value });
+  };
 
   handleLocationFilter = (e) => {
-    this.setState({ locationFilter: e.target.value })
-  }
+    this.setState({ locationFilter: e.target.value });
+  };
 
   handlePriceFilter = (e) => {
-    const priceFilter = e.target.value
-    this.setState({ priceFilter: priceFilter })
-    console.log(priceFilter, "pricefilter")
-  }
+    const priceFilter = e.target.value;
+    this.setState({ priceFilter });
+  };
 
-  handlePriceClick = (e) => {
-    console.log(e.target.value, "price click")
-  }
-
-  renderContent() {
-    const { query, locationFilter, priceFilter } = this.state
-
+  renderFilteredData() {
+    const { query, locationFilter, priceFilter } = this.state;
     const filteredData = this.props.data.filter((singleContent) => {
-      const lowercaseQuery = query.toLowerCase()
-      const lowercaseLocation = singleContent.location.toLowerCase()
+      const lowercaseQuery = query.toLowerCase();
+      const lowercaseLocation = singleContent.location.toLowerCase();
 
       return (
         (singleContent.name.toLowerCase().includes(lowercaseQuery) ||
-          lowercaseQuery.name === "") &&
+          lowercaseQuery === "") &&
         (locationFilter === "" ||
           lowercaseLocation.includes(locationFilter.toLowerCase()))
-      )
-    })
+      );
+    });
 
-    console.log(filteredData)
-
-    if (parseInt(priceFilter) == 0) {
+    if (parseInt(priceFilter) === 0) {
       filteredData.sort((a, b) => {
         return (
           parseInt(a.price.replace(",", "")) -
           parseInt(b.price.replace(",", ""))
-        )
-      })
-    } else if (parseInt(priceFilter) == 1) {
+        );
+      });
+    } else if (parseInt(priceFilter) === 1) {
       filteredData.sort((a, b) => {
         return (
           parseInt(b.price.replace(",", "")) -
           parseInt(a.price.replace(",", ""))
-        )
-      })
+        );
+      });
     }
 
-    console.log(filteredData, "after")
-    let publicUrl = process.env.PUBLIC_URL + "/"
     return filteredData.map((singleContent) => {
       const {
         imagewebp,
@@ -76,13 +66,13 @@ class TourListV3 extends Component {
         name,
         link,
         price,
-      } = singleContent
+      } = singleContent;
 
       return (
-        <div className='col-lg-4 col-sm-6'>
-          <div className='single-destinations-list style-two'>
+        <div className="col-lg-4 col-sm-6" key={link}>
+          <div className="single-destinations-list style-two">
             <Link to={link}>
-              <div className='thumb'>
+              <div className="thumb">
                 <ImageLazyLoad
                   imagejpeg={imagejpeg}
                   imagewebp={imagewebp}
@@ -90,90 +80,68 @@ class TourListV3 extends Component {
                 />
               </div>
 
-              <div className='details'>
-                <p className='location'>
+              <div className="details">
+                <p className="location">
                   <img
                     src={
                       "https://delhibycycle.s3.ap-south-1.amazonaws.com/1.png"
                     }
-                    alt='map'
+                    alt="map"
                   />
                   {location}
                 </p>
-                <h4 className='title'>
+                <h4 className="title">
                   <Link to={link}>{name}</Link>
                 </h4>
-                <p className='content'>{duration}</p>
-                <div className='tp-price-meta'>
+                <p className="content">{duration}</p>
+                <div className="tp-price-meta">
                   <h2>INR {price}</h2>
                 </div>
               </div>
             </Link>
           </div>
         </div>
-      )
-    })
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const { query, locationFilter, priceFilter } = this.state
-
-    // Check if any of the state values have changed
-    if (
-      query !== prevState.query ||
-      locationFilter !== prevState.locationFilter ||
-      priceFilter !== prevState.priceFilter
-    ) {
-      // Call the renderContent() method again
-
-      const updatedContent = this.renderContent()
-
-      // Perform any necessary logic with the updatedContent, if needed
-      this.setState({ updatedContent })
-    }
+      );
+    });
   }
 
   render() {
-    let publicUrl = process.env.PUBLIC_URL + "/"
-    let imagealt = "image"
-    let { query, locationFilter, priceFilter } = this.state
-
-    const { updatedContent } = this.state
+    const { query, locationFilter, priceFilter } = this.state;
 
     return (
-      <div className='tour-list-area pd-top-120 viaje-go-top'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-xl-9 col-lg-8 order-lg-12'>
-              <div className='row justify-content-center'>
-                {updatedContent ? updatedContent : this.renderContent()}
+      <div className="tour-list-area pd-top-120 viaje-go-top">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-9 col-lg-8 order-lg-12">
+              <div className="row justify-content-center">
+                {this.renderFilteredData()}
               </div>
             </div>
-            <div className='col-xl-3 col-lg-4 order-lg-1'>
-              <div className='sidebar-area sidebar-area-inner-page'>
-                <div className='widget tour-list-widget'>
-                  <div className='widget-tour-list-search'>
-                    <form className='search-form'>
-                      <div className='form-group'>
+            <div className="col-xl-3 col-lg-4 order-lg-1">
+              <div className="sidebar-area sidebar-area-inner-page">
+                <div className="widget tour-list-widget">
+                  <div className="widget-tour-list-search">
+                    <form className="search-form">
+                      <div className="form-group">
                         <input
-                          type='text'
+                          type="text"
                           value={query}
                           onChange={this.handleSearch}
-                          placeholder='Search'
+                          placeholder="Search"
                         />
                       </div>
-                      <button className='submit-btn' type='submit'>
-                        <i className='ti-search' />
+                      <button className="submit-btn" type="submit">
+                        <i className="ti-search" />
                       </button>
                     </form>
                   </div>
-                  <div className='widget-tour-list-meta'>
-                    <div className='filter'>
-                      <i className='fa fa-plus-circle' /> Location
+                  <div className="widget-tour-list-meta">
+                    <div className="filter">
+                      <i className="fa fa-plus-circle" /> Location
                     </div>
-                    <div className="filter" >
+                    <div className="filter">
                       <select
-                        className='select w-100 custom-select'
+                        className="select w-100 custom-select"
                         value={locationFilter}
                         onChange={this.handleLocationFilter}
                       >
@@ -187,10 +155,10 @@ class TourListV3 extends Component {
                     </div>
 
                     <div className="filter">
-                      <i className='fa fa-plus-circle' /> Travel Type
+                      <i className="fa fa-plus-circle" /> Travel Type
                     </div>
-                    <div className="filter" >
-                      <select className='select w-100 custom-select'>
+                    <div className="filter">
+                      <select className="select w-100 custom-select">
                         <option value={1}>Day Cycle Tour</option>
                         <option value={2}>Walking Tour</option>
                         <option value={3}>Cycling Holidays</option>
@@ -198,29 +166,17 @@ class TourListV3 extends Component {
                     </div>
 
                     <div className="filter">
-                      <i className='fa fa-usd' /> Price Filter
+                      <i className="fa fa-usd" /> Price Filter
                     </div>
                     <div className="filter">
                       <select
-                        className='select w-100 custom-select'
+                        className="select w-100 custom-select"
                         value={priceFilter}
                         onChange={this.handlePriceFilter}
                       >
                         <option value={0}>Low - High</option>
                         <option value={1}>High - Low</option>
                       </select>
-                      {/* <option value={true}>Low - High</option>
-                        <option value={3500}> Under 3000</option>
-                        <option value={21000}>Low - High</option>
-                        {/* <option value={3500}> Under 3000</option>
-                        <option value={21000}> 3000 - 20000</option>
-                        <option value={76000}>Above 20000 </option>
-                      </select> */}
-                      {/* <div className="slider-product-sorting" />
-                      <div className="product-range-detail">
-                        <label htmlFor="amount">Price: </label>
-                        <input type="text" id="amount" readOnly />
-                      </div> */}
                     </div>
                   </div>
                 </div>
@@ -229,8 +185,8 @@ class TourListV3 extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default TourListV3
+export default TourListV3;
